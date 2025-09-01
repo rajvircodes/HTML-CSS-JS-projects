1bb2d37ea1e1947d52db7f471e01e3003548c43a
rajvircodes <rajvirsinhgohil004@gmail.com>
2025-09-01 16:31:37 +0530

    to do list app

---
+++ 05-To-Do-list/script.js
@@ -0,0 +1,25 @@
+const addTaskbtn = document.getElementById("add-task-btn");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+addTaskbtn.addEventListener("click",addTask);
+function addTask(){
+    const taskText = taskInput.value.trim();
+   
+    if(taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className = "delete-btn";
+    deleteBtn.addEventListener("click", () =>{
+        li.remove();
+    });
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    taskInput.value = " ";
+}
+
+
