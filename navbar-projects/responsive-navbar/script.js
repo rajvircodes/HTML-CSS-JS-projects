1b3e09de1743156fe2336ad2edd05c4aea49464a
rajvircodes <rajvirsinhgohil004@gmail.com>
2025-09-16 08:33:31 +0530

    some changes

--- navbar-projects/responsive-navbar/script.js
+++
@@ -1,6 +0,0 @@
-const mobilemenu = document.getElementById("mobile-menu");
-const navLinks  = document.querySelector (".nav-links");
-
-mobilemenu.addEventListener("click",() => {
-    navLinks.classList.toggle("active");
-})
--- navbar-projects/sticky-navbar/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-    let navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50){
-        navbar.classList.add("sticky");
-    }
-    else {
-        navbar.classList.remove("sticky");
-    }
-});
